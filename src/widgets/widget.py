b73585976d7674e1f62d521848f0e6673ab3d965
Wrench56 <dmarkreg@gmail.com>
2022-10-22 22:32:09 +0200

    Updated widget base class (added a better way to do resizing)

--- src/widgets/widget.py
+++ src/widgets/widget.py
@@ -5,9 +5,19 @@ class Widget():
         self.lambda_x = lambda x: x
         self.lambda_y = lambda y: y
 
-    def set_size(self, lambda_x, lambda_y):
+    def set_size(self, lambda_x, lambda_y, lambda_w, lambda_h):
         self.lambda_x = lambda_x
         self.lambda_y = lambda_y
+        self.lambda_w = lambda_w
+        self.lambda_h = lambda_h
 
-    def draw(self, x, y):
-        pass
+    def getxy(self):
+        return self.last_x, self.last_y
+
+    def resize(self, x, y):
+        self.last_x = x
+        self.last_y = y
+
+        self.draw()
+
+    

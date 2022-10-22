775fd8d114b606116b84769d036e2e618384482c
Wrench56 <dmarkreg@gmail.com>
2022-10-22 22:33:51 +0200

    Added color support & MultilineLabel()

--- src/widgets/label.py
+++ src/widgets/label.py
@@ -1,21 +1,36 @@
-from widgets import widget
-import math
+import curses
 
+from widgets import widget
+from utils import colors
 class Label(widget.Widget):
-    def __init__(self, stdscr, text):
+    def __init__(self, stdscr, text, color=None):
         self.text = text
+
+        if color == None:
+            self.color = curses.color_pair(colors.WHITE_ON_BLACK)
+        else:
+            self.color = color
+        
         super().__init__(stdscr)
 
-    def draw(self, x, y):
-        self.last_x = x
-        self.last_y = y
+    def draw(self):
+        x, y = super().getxy()
 
-        self.stdscr.addstr(self.lambda_y(y), self.lambda_x(x)-self.calculate_text_size(self.text), self.text)
+        self.stdscr.addstr(self.lambda_y(y), self.lambda_x(x), self.text, self.color)
         self.stdscr.refresh()
-
-    def calculate_text_size(self, text):
-        return math.floor(len(text) / 2)
 
     def set_text(self, text):
         self.text = text
         self.draw(self.last_x, self.last_y)
+
+
+class MultilineLabel(Label):
+    def __init__(self, stdscr, text, color=None):
+        super().__init__(stdscr, text, color=color)
+
+    def draw(self):
+        x, y = super().getxy()
+        for i, line in enumerate(self.text.split('\n')):
+            self.stdscr.addstr(self.lambda_y(y)+i, self.lambda_x(x), f'{line}\n', self.color)
+            
+        self.stdscr.refresh()

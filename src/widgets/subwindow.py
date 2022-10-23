c32a0821a42edef032ce10ad37698a0af4fd4670
Wrench56 <dmarkreg@gmail.com>
2022-10-23 17:54:35 +0200

    Solved major Subwindow() resize bug

--- src/widgets/subwindow.py
+++ src/widgets/subwindow.py
@@ -12,7 +12,7 @@ class Subwindow(widget.Widget):
         self.window.mvwin(0, 0) #! Do NOT delete this: this saves you from a lot of curses error
         self.window.resize(1, 1) #! Do NOT delete this: this saves you from a lot of curses error
         self.window.mvwin(self.lambda_y(y), self.lambda_x(x))
-        self.window.resize(self.lambda_h(x), self.lambda_w(x))
+        self.window.resize(self.lambda_h(y), self.lambda_w(x))
         if self.border:
             self.window.border(0)
         self.window.refresh()

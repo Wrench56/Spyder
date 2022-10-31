c7c399b1afaef6e04318602a5d2cb081a81ec74a
Wrench56 <dmarkreg@gmail.com>
2022-10-31 23:16:00 +0100

    Added rndinjection to server's encryption wrappers

---
+++ server/network/encryption/rndinjection.py
@@ -0,0 +1,17 @@
+import math
+import os
+
+# This module is used to obscure constant strings.
+# Else the encrypted constant string would be a huge security vulnerability 
+
+def encrypt(string: str):
+    encrypted = ''
+    for char in string:
+        encrypted += char + chr(ord(os.urandom(1)))
+    return encrypted
+
+def decrypt(string: str):
+    decrypted = ''
+    for x in range(math.floor(len(string)/2)):
+        decrypted += str(string[x*2])
+    return decrypted

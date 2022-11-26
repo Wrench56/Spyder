3382b3f736e5d7ed05647646cee51ef0a93532fe
Wrench56 <dmarkreg@gmail.com>
2022-11-26 20:38:16 +0100

    Moved symmetric.py (part of the structural upgrade)

---
+++ src/provider/encryption/symmetric.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+def generate():
+    return Fernet.generate_key()
+
+def convert(key):
+    return Fernet(key)
+
+def encrypt(string: bytes, key):
+    return key.encrypt(string)
+
+def decrypt(string: bytes, key):
+    return key.decrypt(string)

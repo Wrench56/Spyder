45eba5974ec6843011e24fede4804482a478f142
Wrench56 <dmarkreg@gmail.com>
2022-12-15 23:40:23 +0100

    Added unsubscribe() function for on_resize event

--- src/client/events/on_resize.py
+++ src/client/events/on_resize.py
@@ -3,6 +3,9 @@ _subscribed = []
 def subscribe(function):
     _subscribed.append(function)
 
+def unsubscribe(function):
+    del _subscribed[_subscribed.index(function)] # KeyError: function was not subscribed
+
 def trigger(x_max, y_max):
     for function in _subscribed:
         function(x_max, y_max)

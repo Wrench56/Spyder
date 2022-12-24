3adb89ab50e461241c4d4feb267ed44c09843f2a
Wrench56 <dmarkreg@gmail.com>
2022-12-24 15:33:22 +0100

    Removed non-ANSI colored string support for label

--- src/client/widgets/label.py
+++ src/client/widgets/label.py
@@ -3,24 +3,13 @@ import curses
 from widgets import widget
 from utils import colors
 class Label(widget.Widget):
-    def __init__(self, stdscr, text, color=None, ansi_color=False):
+    def __init__(self, stdscr, text):
         self.text = text
-
-        if color == None:
-            self.color = curses.color_pair(colors.WHITE_ON_BLACK)
-        else:
-            self.color = color
-
-        self.ansi_color = ansi_color
-        
         super().__init__(stdscr)
 
     def draw(self):
         x, y = super().getxy()
-        if self.ansi_color:
-            colors.colored_addstr(self.stdscr, self.lambda_x(x), self.lambda_y(y), self.text)
-        else:
-            self.stdscr.addstr(self.lambda_y(y), self.lambda_x(x), self.text, self.color)
+        colors.colored_addstr(self.stdscr, self.lambda_x(x), self.lambda_y(y), self.text)
 
         self.stdscr.refresh()
 
@@ -30,15 +19,12 @@ class Label(widget.Widget):
 
 
 class MultilineLabel(Label):
-    def __init__(self, stdscr, text, color=None):
-        super().__init__(stdscr, text, color=color)
+    def __init__(self, stdscr, text):
+        super().__init__(stdscr, text)
 
     def draw(self):
         x, y = super().getxy()
         for i, line in enumerate(self.text.split('\n')):
-            if self.ansi_color:
-                colors.colored_addstr(self.stdscr, self.lambda_x(x), self.lambda_y(y), self.text)
-            else:
-                self.stdscr.addstr(self.lambda_y(y)+i, self.lambda_x(x), f'{line}\n', self.color)
+            colors.colored_addstr(self.stdscr, self.lambda_x(x), self.lambda_y(y)+i, str(line))
             
         self.stdscr.refresh()

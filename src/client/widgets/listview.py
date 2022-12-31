4c01843f5c06db99c6aa272a538745758180b995
Wrench56 <dmarkreg@gmail.com>
2022-12-31 05:10:13 +0100

    Separated mouse area handling from mouse input handling

--- src/client/widgets/listview.py
+++ src/client/widgets/listview.py
@@ -50,21 +50,21 @@ class ListView(widget.Widget):
         elif key == keyboard.KEY_ENTER:
             return self.selected_item()
         elif key == curses.KEY_MOUSE:
-            return self.handle_mouse_input()
+            x, y = self.getxy()
+            mouse_event = curses.getmouse()
+            if self.lambda_x(x) <= mouse_event[1] and self.lambda_x(x)+self.lambda_w(x) >= mouse_event[1]:
+                if self.lambda_y(y) <= mouse_event[2] and self.lambda_y(y)+self.lambda_h(y) >= mouse_event[2]:
+                    self.handle_mouse_input(mouse_event, x, y)
 
     def selected_item(self):
         return self.buffer[self.cursor]
-
-    def handle_mouse_input(self):
-        x, y = self.getxy()
-        mouse_event = curses.getmouse()
-        if self.lambda_x(x) <= mouse_event[1] and self.lambda_x(x)+self.lambda_w(x) >= mouse_event[1]:
-            if self.lambda_y(y) <= mouse_event[2] and self.lambda_y(y)+self.lambda_h(y) >= mouse_event[2]:
-                if mouse_event[4] == curses.BUTTON1_CLICKED:
-                    try:
-                        return self.buffer[self.pad_pos_y+mouse_event[2]-self.lambda_x(x)-1]
-                    except IndexError:
-                        pass
+    
+    def handle_mouse_input(self, mouse_event, x, y):
+        if mouse_event[4] == curses.BUTTON1_CLICKED:
+            try:
+                return self.buffer[self.pad_pos_y+mouse_event[2]-self.lambda_x(x)-1]
+            except IndexError:
+                pass
 
     def add_new(self, name: str):
         self.buffer.append(name)

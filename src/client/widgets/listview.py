a64de84cf6d3b8c21af61bfbe9e866237f2caead
Wrench56 <dmarkreg@gmail.com>
2022-12-31 04:35:24 +0100

    Removed listview_utils and restored ListView (a new class is going to be made for node-like behavior support)

--- src/client/widgets/listview.py
+++ src/client/widgets/listview.py
@@ -1,13 +1,14 @@
-from widgets import widget, label, subwindow, listview_utils
+from widgets import listview_node, widget
 from utils import keyboard, colors
 
 import curses
 
+
 class ListView(widget.Widget):
-    def __init__(self, stdscr, width=20, height=100, border=False):
+    def __init__(self, stdscr, width=20, height=100):
         super().__init__(stdscr)
 
-        self.buffer = ['Hello', [True, 'Friend', 'World', [False, 'Universe', 'Multiverse']], 'Mark', [True, 'Wrd', [True, 'A', 'B'], 'CRD']]
+        self.buffer = ['Hello', 'World']
 
         self.pad_pos_x = 0
         self.pad_pos_y = 0
@@ -19,7 +20,6 @@ class ListView(widget.Widget):
 
         self.pad = curses.newpad(height, width)
         self.pad.scrollok(True)
-        
 
     def draw(self):
         x, y = super().getxy()
@@ -27,10 +27,14 @@ class ListView(widget.Widget):
         lx = self.lambda_x(x)
 
         self.pad.erase()
-        listview_utils.draw_tree(self.pad, self.buffer, 0, '')
+        self.draw_items()
 
         sy, sx = self.stdscr.getbegyx()
         self.pad.refresh(self.pad_pos_y, self.pad_pos_x, sy+ly, sx+lx, sy+ly+self.lambda_h(y), sx+lx+self.lambda_w(x))
+
+    def draw_items(self):
+        for i, item in enumerate(self.buffer):
+            colors.colored_addstr(self.pad, 0, i, item)
 
     def input(self, key) -> None|str:
         if key == curses.KEY_DOWN:
@@ -44,12 +48,12 @@ class ListView(widget.Widget):
             self.cursor -= 1
             self.draw()
         elif key == keyboard.KEY_ENTER:
-            if not listview_utils.toggle_expand(self.buffer, self.cursor):
-                return listview_utils.flatten(self.buffer)[self.cursor]
-            else:
-               self.draw()
+            return self.selected_item()
         elif key == curses.KEY_MOUSE:
             return self.handle_mouse_input()
+
+    def selected_item(self):
+        return self.buffer[self.cursor]
 
     def handle_mouse_input(self):
         x, y = self.getxy()
@@ -58,18 +62,13 @@ class ListView(widget.Widget):
             if self.lambda_y(y) <= mouse_event[2] and self.lambda_y(y)+self.lambda_h(y) >= mouse_event[2]:
                 if mouse_event[4] == curses.BUTTON1_CLICKED:
                     try:
-                        clicked_node = listview_utils.flatten(self.buffer)[self.pad_pos_y+mouse_event[2]-self.lambda_x(x)-1]
-                        if not listview_utils.toggle_expand(self.buffer, self.pad_pos_y+mouse_event[2]-self.lambda_x(x)-1):
-                            return clicked_node
-                        else:
-                            self.draw()
+                        return self.buffer[self.pad_pos_y+mouse_event[2]-self.lambda_x(x)-1]
                     except IndexError:
                         pass
-    
-    def add_node(self, name, path: str = '', subnodes: list = []):
-        listview_utils.add_node(self.buffer, name, path, subnodes)
 
-    
+    def add_new(self, name: str):
+        self.buffer.append(name)
+
     def set_buffer(self, buff):
         self.buffer = buff
         self.draw()

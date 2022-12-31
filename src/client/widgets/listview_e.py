19c67f051b6cf50ced390e2e7a26eae206fe0fc3
Wrench56 <dmarkreg@gmail.com>
2022-12-31 05:12:32 +0100

    Added extended listview LisViewE() & its Node() class

---
+++ src/client/widgets/listview_e.py
@@ -0,0 +1,35 @@
+from widgets.listview import ListView
+from widgets.listview_node import Node
+
+import curses
+
+
+class ListViewE(ListView):
+    def __init__(self, stdscr, width=20, height=100):
+        super().__init__(stdscr, width, height)
+        self.buffer = [Node('Hello', [Node('A', [Node('1'), Node('2')], False), Node('B', [Node('1'), Node('2')])])]
+
+    def draw_items(self):
+        line = 0
+        for item in self.buffer:
+            line = item.draw(self.pad, line)
+
+    def handle_mouse_input(self, mouse_event, x, y):
+        if mouse_event[4] == curses.BUTTON1_CLICKED:
+            result = 0
+            for node in self.buffer:
+                result = node.get_by_index(self.pad_pos_y+mouse_event[2]-self.lambda_x(x), result)
+                if isinstance(result, str):
+                    return result
+                if isinstance(result, bool):
+                    break
+            self.draw()
+
+    def get_item(self, path: str):
+        path_segments = path.split('/')
+        for item in self.buffer:
+            if item.name == path_segments[0]:
+                return item.get(path_segments[1:])
+
+    def add_new(self, path: str, node: Node):
+        self.get_item(path).add_node(node)
---
+++ src/client/widgets/listview_node.py
@@ -0,0 +1,64 @@
+from utils.colors import colored_addstr
+
+
+class Node:
+    def __init__(self, name, nodes=[], is_expanded=True) -> None:
+        self.name: str = name
+
+        self.nodes: list = nodes
+        self.is_expanded: bool = is_expanded
+
+    def toggle_state(self):
+        if len(self.nodes) > 0:
+            self.is_expanded = False if self.is_expanded else True
+            return True
+        return False
+
+    def add_node(self, node_obj: object):
+        self.nodes.append(node_obj)
+
+    def get_node(self, path: list):
+        for node in self.nodes:
+            if node.name == path[0]:
+                return node.get_node(path[1:])
+        return False
+
+    def get_by_index(self, f_index: int, c_index: int, path: str = ''):
+        path += self.name
+        c_index += 1
+        if c_index == f_index:
+            if len(self.nodes) > 0:
+                self.toggle_state()
+                return self.is_expanded
+            return path
+
+        if self.is_expanded:
+            path += '/'
+            for node in self.nodes:
+                result = node.get_by_index(f_index, c_index, path)
+                if isinstance(result, bool) or isinstance(result, str):
+                    return result
+
+                c_index = result
+        return c_index
+
+    def draw(self, pad, line: int, tab: str = ''):
+        if len(self.nodes) > 0:
+            if self.is_expanded:
+                ec_char = '▾'
+            else:
+                ec_char = '▸'
+        else:
+            ec_char = '╴'
+        colored_addstr(pad, 0, line, f'{tab}{ec_char}{self.name}')
+        line += 1
+        if not self.is_expanded:
+            return line
+        for i, node in enumerate(self.nodes):
+            if len(self.nodes)-1 == i:
+                new_tab = '└'
+            else:
+                new_tab = '├'
+            line = node.draw(pad, line, tab=(tab.replace('├', '│').replace('└', ' ')+new_tab))
+
+        return line

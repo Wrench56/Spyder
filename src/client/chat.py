8592ccda5eb8748604abf35829ba6d415b75d881
Wrench56 <dmarkreg@gmail.com>
2023-01-05 00:13:37 +0100

    Fixed flake8 stylistic errors for chat.py

--- src/client/chat.py
+++ src/client/chat.py
@@ -2,10 +2,8 @@ from screens import chat
 from plugins import handler
 from events import on_start
 
+
 def main(stdscr, login_data):
     handler.load_plugins()
-
-    chat_screen = chat.Chat(stdscr)    
-    #chat_screen.logic()
+    chat.Chat(stdscr)
     on_start.trigger()
-

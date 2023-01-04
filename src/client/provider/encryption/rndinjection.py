4349717f91d2470ba8d39966399d6386f2eaf5ec
Wrench56 <dmarkreg@gmail.com>
2023-01-05 00:26:09 +0100

    Fixed flake8 stylistic errors for all "providers" including encryption utilities

--- src/client/provider/encryption/rndinjection.py
+++ src/client/provider/encryption/rndinjection.py
@@ -2,7 +2,8 @@ import math
 import os
 
 # This module is used to obscure constant strings.
-# Else the encrypted constant string would be a huge security vulnerability 
+# Else the encrypted constant string would be a huge security vulnerability
+
 
 def encrypt(string: str):
     encrypted = ''
@@ -10,8 +11,9 @@ def encrypt(string: str):
         encrypted += char + chr(ord(os.urandom(1)))
     return encrypted
 
+
 def decrypt(string: str):
     decrypted = ''
-    for x in range(math.floor(len(string)/2)):
-        decrypted += str(string[x*2])
+    for x in range(math.floor(len(string) / 2)):
+        decrypted += str(string[x * 2])
     return decrypted
--- src/client/provider/encryption/symmetric.py
+++ src/client/provider/encryption/symmetric.py
@@ -1,13 +1,17 @@
 from cryptography.fernet import Fernet
 
+
 def generate():
     return Fernet.generate_key()
+
 
 def convert(key):
     return Fernet(key)
 
+
 def encrypt(string: bytes, key):
     return key.encrypt(string)
+
 
 def decrypt(string: bytes, key):
     return key.decrypt(string)

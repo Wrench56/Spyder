f44cdf6a375f7924673c9e3a8f98ea0e43470ed7
Wrench56 <dmarkreg@gmail.com>
2023-01-05 21:12:07 +0100

    Added type hints for all events

--- src/client/events/on_resize.py
+++ src/client/events/on_resize.py
@@ -1,14 +1,16 @@
-_subscribed = []
+from typing import List, Callable
+
+_subscribed: List[Callable[[int, int], None]] = []
 
 
-def subscribe(function):
+def subscribe(function: Callable[[int, int], None]) -> None:
     _subscribed.append(function)
 
 
-def unsubscribe(function):
+def unsubscribe(function: Callable[[int, int], None]) -> None:
     del _subscribed[_subscribed.index(function)]  # KeyError: function was not subscribed
 
 
-def trigger(x_max, y_max):
+def trigger(x_max: int, y_max: int) -> None:
     for function in _subscribed:
         function(x_max, y_max)

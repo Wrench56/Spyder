e2438c4b5d10ea591c5de0cc5e4ef7b356011907
Wrench56 <dmarkreg@gmail.com>
2023-08-27 14:24:16 -0400

    Ignore W503 in flake8 (line break before binary operator warning)

--- tools/lint.sh
+++ tools/lint.sh
@@ -2,7 +2,7 @@
 
 # Do all the linting on your machine
 
-flake8 src/client --ignore=E501
+flake8 src/client --ignore=E501,W503
 pylint src/client --recursive True --rcfile=.pylintrc --reports True
 mypy src/client --config-file=.mypy.ini
 bandit src/client -r

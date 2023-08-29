4576a77804d43aab6d205f7b8879bd58166980e5
Wrench56 <dmarkreg@gmail.com>
2023-08-29 17:10:37 -0400

    Colorize pylint output for client-lint workflow & lint.sh

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,6 +3,6 @@
 # Do all the linting on your machine
 
 flake8 src/client --ignore=E501,W503
-pylint src/client --recursive True --rcfile=.pylintrc --reports True
+pylint src/client --recursive True --rcfile=.pylintrc --reports True --output-format=colorized
 mypy src/client --config-file=.mypy.ini
 bandit src/client -r

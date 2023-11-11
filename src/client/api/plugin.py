30951bf14af3542624c041ef16be59ee66614cff
Wrench56 <dmarkreg@gmail.com>
2023-11-10 20:28:42 -0500

    Add "plugin" API (register/unregister/get plugins)

--- src/client/api/plugin.py
+++ src/client/api/plugin.py
@@ -1,43 +1,82 @@
 """
-This module is for the "plugin manager" plugin ONLY!
-
-In this module there are functions to import and
-manage extensions/plugins from another plugin. This API
-module should be exposed to one plugin only, the "plugin manager"
+This module is for plugins to make themselves public for other plugins.
 
 Exposed functions:
-    - import_plugin
+    - register
+    - unregister
+    - get
+    - exists
 """
 
-from typing import Optional
-import importlib
+from typing import Dict, Optional
 import logging
 
-from plugins.base_plugin import Plugin
+from plugins import base_plugin
 
 
-def import_plugin(name: str) -> Optional[Plugin]:
+_plugins: Dict[str, base_plugin.Plugin] = {}
+
+def register(name: str, plugin: base_plugin.Plugin) -> bool:
     """
-    Import a plugin with importlib and return the Plugin() object.
+    Register a plugin
+
+    Args:
+        name: Name of the plugin. Others can access the plugin
+              through this name.
+        plugin: The plugin object itself
+
+    Returns:
+        bool: True if the plugin was registered successfully, else False
+    """
+    if name not in _plugins:
+        _plugins[name] = plugin
+        return True
+
+    logging.error(f'Plugin with name "{name}" already registered')
+    return False
+
+def unregister(name: str) -> bool:
+    """
+    Un-register a plugin
+
+    Args:
+        name: Name of the plugin to unregister
+    Returns:
+        bool: True if the plugin was unregistered successfully
+              and else False
+    """
+    if name in _plugins:
+        del _plugins[name]
+        return True
+
+    logging.error(f'Plugin "{name}" cannot be unregistered as it never was registered')
+    return False
+
+
+def get(name: str) -> Optional[base_plugin.Plugin]:
+    """
+    Return a plugin by name
 
     Args:
         name: Name of the plugin
-
     Returns:
-        None | Plugin: Returns Plugin() object if import was successful
-                       else None
+        Plugin: Returns the plugin object itself
     """
-    try:
-        plugin: Plugin = importlib.import_module(f'plugins.{name}.main').init()
-        return plugin
-    except TypeError:
-        # Abstract class (Plugin) does not implement methods like load & unload
-        logging.error(f'Plugin "{name}" does not implement abstract methods')
-    except AttributeError:
-        # No init() function
-        logging.error(f'Plugin "{name}" does not provide an init() function')
-    except ModuleNotFoundError:
-        # No such file
-        logging.error(f'Plugin "{name}" does not exist')
+    if name in _plugins:
+        return _plugins[name]
 
+    logging.error(f'The plugin {name} you tried to get does not exist')
     return None
+
+def exists(name: str) -> bool:
+    """
+    Check whether the a plugin is registered
+
+    Args:
+        name: Name of the plugin
+    Returns:
+        bool: True if the plugin exists, else False
+    """
+    if name in _plugins:
+        return True
+    return False
